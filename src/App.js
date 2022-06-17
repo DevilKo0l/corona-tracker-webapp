b7d56fa144f4b680b7028dd8fcbf1258da9bcaf2
DevilKo0l <nguyen.tcao96@gmail.com>
2022-06-17 23:13:56 +0200

    Update Api usage

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from "react";
 
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
+import { fetchData } from "./api";
 class App extends React.Component {
+  state = {
+    data: {},
+  };
+  async componentDidMount() {
+    const fetchData = await fetchData();
+    this.setState({ data: fetchData });
+  }
   render() {
+    const { data } = this.setState;
     return (
       <div className={styles.container}>
-        <h1>
-          <Cards />
-          <CountryPicker />
-          <Chart />
-        </h1>
+        <Cards data={data} />
+        <CountryPicker />
+        <Chart />
       </div>
     );
   }
