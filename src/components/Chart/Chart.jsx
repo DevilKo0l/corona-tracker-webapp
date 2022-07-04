e49319798f726b8b92a8057730da07d8dad2fda0
DevilKo0l <nguyen.tcao96@gmail.com>
2022-07-04 17:51:35 +0200

    downgrade version

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,28 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { fetchDailyData } from "../../api";
 import styles from "./Chart.module.css";
-
 import { Line, Bar } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
 
 const Chart = ({ data: { confirmed, deaths, recovered }, country }) => {
   const [dailyData, setDailyData] = useState([]);
